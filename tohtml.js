caa0ab2ba4a0ecab5bce81e41ba28a5a77897141
Cat McGee <hello@mcgee.cat>
2024-07-16 11:50:35 +0100

    no dependencies

--- tohtml.js
+++ tohtml.js
@@ -1,10 +1,34 @@
 const fs = require('fs');
-const marked = require('marked');
 
+function enhancedMarkdownToHtml(markdown) {
+  return markdown
+    // Headers
+    .replace(/^### (.*$)/gm, '<h3>$1</h3>')
+    .replace(/^#### (.*$)/gm, '<h4>$1</h4>')
+    .replace(/^##### (.*$)/gm, '<h5>$1</h5>')
+    // Bold
+    .replace(/\*\*(.*)\*\*/gm, '<strong>$1</strong>')
+    // Italic
+    .replace(/\*(.*)\*/gm, '<em>$1</em>')
+    // Links
+    .replace(/\[(.*?)\]\((.*?)\)/gm, '<a href="$2">$1</a>')
+    // Lists
+    .replace(/^\s*[\-\*] (.*)/gm, '<li>$1</li>')
+    // Wrap lists in <ul> tags
+    .replace(/(<li>.*<\/li>)/s, '<ul>$1</ul>')
+    // Preserve emojis (this is a simple approach, might not cover all cases)
+    .replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '&#x$1;')
+    // Line breaks
+    .replace(/\n/gm, '<br>');
+}
+
+// Read the README.md file
 const readmeContent = fs.readFileSync('README.md', 'utf-8');
 
-const htmlContent = marked.parse(readmeContent);
+// Convert Markdown to HTML
+const htmlContent = enhancedMarkdownToHtml(readmeContent);
 
+// Create a simple HTML template with emoji favicon and updated title
 const htmlTemplate = `
 <!DOCTYPE html>
 <html lang="en">
@@ -21,6 +45,9 @@ const htmlTemplate = `
             max-width: 800px;
             margin: 0 auto;
         }
+        ul {
+            padding-left: 20px;
+        }
     </style>
 </head>
 <body>
@@ -29,6 +56,7 @@ const htmlTemplate = `
 </html>
 `;
 
+// Write the HTML content to index.html
 fs.writeFileSync('index.html', htmlTemplate);
 
 console.log('index.html has been created successfully!');
